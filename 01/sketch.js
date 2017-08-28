ecb775a7a21a1dc5ece42d33734fe90fab379a37
presudo <31419369+presudo@users.noreply.github.com>
2017-08-28 15:00:49 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,72 @@
+function setup() { 
+
+  createCanvas(1000, 1000);
+	background(50,120,200);
+	
+	beginShape();
+	fill(220,10,50);
+	strokeWeight(5);
+	stroke(40);
+	vertex(150,150);
+	vertex(900,600);
+	vertex(100,600);
+	vertex(850,150);
+	vertex(500,900);
+	vertex(150,150);
+	endShape();
+	
+		beginShape();
+	noFill();
+	strokeWeight(5);
+	stroke(40);
+	vertex(480,100);
+	vertex(440,150);
+	vertex(530,150);
+	vertex(470,200);
+	endShape();
+	
+	beginShape();
+	noFill();
+	strokeWeight(5);
+	stroke(40);
+	vertex(800,450);
+	vertex(830,350);	
+vertex(860,450);
+	vertex(790,400);
+	endShape();
+	
+	beginShape();
+	noFill();
+	strokeWeight(5);
+	stroke(40);
+	vertex(700,720);
+	vertex(780,720);
+	vertex(740,720);
+	vertex(740,800);  
+	endShape();
+	
+		beginShape();
+	noFill();
+	strokeWeight(5);
+	stroke(40);
+	vertex(200,800);
+	vertex(230,700);
+	vertex(260,800);
+	vertex(190,750);
+	endShape();
+	
+		beginShape();
+	noFill();
+	strokeWeight(5);
+	stroke(40);
+	vertex(150,480);
+	vertex(150,400);
+	vertex(200,480);
+	vertex(200,400);
+	
+	
+	
+	endShape();
+	
+	
+} 
